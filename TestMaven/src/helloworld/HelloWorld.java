1192810cf8926ac3467bb457f9cbddc19e2ccc29
Usuario <Usuario@Usuario-PC>
2021-04-20 15:11:16 +0200

    Well, this wasn't that hard

--- TestMaven/src/helloworld/HelloWorld.java
+++ TestMaven/src/helloworld/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello world!");
 		System.out.println("Hello world: Electric Boogaloo!");
 		System.out.println("Hello world 3");
+		System.out.println("And hopefully last hello world!");
 	}
 
 }
