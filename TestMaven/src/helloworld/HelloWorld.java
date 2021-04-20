911211cd59a7df307edffb8aa865a28dd93ea76b
Usuario <Usuario@Usuario-PC>
2021-04-20 15:10:45 +0200

    yet another hello world

--- TestMaven/src/helloworld/HelloWorld.java
+++ TestMaven/src/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String... args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello world: Electric Boogaloo!");
+		System.out.println("Hello world 3");
 	}
 
 }
